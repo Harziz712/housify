598fc4b7922b18b4e1a8d002d60d95097e79f91f
Akanbi AbdulAzeez <akanbiazeez117@gmail.com>
2025-03-28 20:57:48 +0100

    file arangement

---
+++ src/assets/images.tsx
@@ -0,0 +1,9 @@
+import aboutpic from "./aboutpic.jpg"
+import house1 from "./house1.jpg"
+import housels from "./houseLs.jpg"
+import property1 from "./property1.jpg"
+
+
+export const images = {
+    aboutpic, house1,housels, property1
+}
